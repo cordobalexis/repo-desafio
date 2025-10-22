c9a44b55062d4a02cb92f0ae2680c9983cafd12c
Alexis <alexiscordoba978@gmail.com>
2025-10-21 21:20:59 -0300

    primer commit

---
+++ index.ts
@@ -0,0 +1,30 @@
+class Departamento {
+    name:string;
+    constructor(name:string){
+        this.name = name;
+    }
+    getName() {
+return this.name;
+    }
+}
+
+class Piso {
+       nombre:string;
+       departamentos:Departamento[];
+    constructor(nombre:string){
+        this.nombre = nombre;
+        this.departamentos = [];
+    }
+    pushDepartamento(departamentos:Departamento) {
+   this.departamentos.push(departamentos);
+    }
+    getDepartamentos(){
+        return this.departamentos;
+    }
+}
+class Edificio {
+    piso:Piso[];
+    constructor(piso:Piso[]){
+this.piso = piso;
+    }
+}
